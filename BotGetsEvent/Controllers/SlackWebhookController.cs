64b1bddc8535523ddf4d75034ce8bbf1480be51d
ArisaNiitsuma <d01tsumath@sigmact.com>
2020-02-03 01:38:44 +0900

    API の実装を変更

--- BotGetsEvent/Controllers/SlackWebhookController.cs
+++ BotGetsEvent/Controllers/SlackWebhookController.cs
@@ -18,29 +18,63 @@ namespace BotGetsEvent.Controllers
     /// </summary>
     public class SlackWebhookController
     {
+        #region 定数
+        /// <summary>
+        /// Queue 名を表します。
+        /// </summary>
+        private const string QueueName = "slack-message";
+        #endregion
+
+        private StorageQueueService _queueService;
+
         private BotService _botService;
 
-        public SlackWebhookController(BotService botService)
+        public SlackWebhookController(StorageQueueService queueService, BotService botService)
         {
+            _queueService = queueService;
             _botService = botService;
         }
 
-        [FunctionName("Api_ReturnMessage")]
+        [FunctionName("Http_ReturnMessage")]
         public async ValueTask<IActionResult> ReturnMessageAsync(
             [HttpTrigger(AuthorizationLevel.Function, "post", Route = null)] HttpRequest request,
             ILogger log)
         {
-            var requestBody = await new StreamReader(request.Body).ReadToEndAsync();
-            var json = JsonSerializer.Deserialize<EventRequestModel>(requestBody);
+            try
+            {
+                var requestBody = await new StreamReader(request.Body).ReadToEndAsync();
+                var json = JsonSerializer.Deserialize<EventRequestModel>(requestBody);
+
+                // 中身確認したい
+                log.LogInformation($"token:{json.Token}, challenge:{json.Challenge}, type:{json.Type}");
+                
+                if (json.Type.Equals("url_verification"))
+                {
+                    // "url_verification" ならそのまま challenge の値を返す
+                    return new ObjectResult(JsonSerializer.Serialize(new { challenge = json.Challenge }));
+                }
+
+                // "url_verification" 以外なら Queue へ投げる
+                await _queueService.AddAsync(QueueName, requestBody).ConfigureAwait(false);
+                return new OkObjectResult("");
+            }
+            catch (Exception ex)
+            {
+                log.LogError(ex, "Event Callback の処理に失敗しました。");
+                return new ObjectResult(ex);
+            }
+
+        }
+
+        /// <summary>
+        /// Slack の Event API のデータを処理します。
+        /// </summary>
+        /// <returns></returns>
+        [FunctionName("Queue_ProcessMessage")]
+        public async Task ProcessPayload(
+            [QueueTrigger(QueueName)]CloudQueueMessage message)
+        {
             
-            // 中身確認したい
-            log.LogInformation($"token:{json.Token}, challenge:{json.Challenge}, type:{json.Type}");
-
-            // Bot からメッセージをしゃべらせる
-            await _botService.SpeakMessageAsync(log);
-
-            // そのまま challenge の値を返す
-            return new ObjectResult(JsonSerializer.Serialize(new { challenge = json.Challenge }));
         }
 
     }
