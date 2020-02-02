7f10218770861b7e21d2b5388c140126d84648b2
ArisaNiitsuma <d01tsumath@sigmact.com>
2020-02-03 01:35:50 +0900

    通常メッセージのモデルを追加

---
+++ BotGetsEvent/Models/Events.cs
@@ -0,0 +1,49 @@
+﻿using System.Text.Json.Serialization;
+
+namespace BotGetsEvent.Models
+{
+    public class Events
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        [JsonPropertyName("type")]
+        public string Type { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        [JsonPropertyName("channel")]
+        public string Channel { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        [JsonPropertyName("user")]
+        public string User { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        [JsonPropertyName("text")]
+        public string Text { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        [JsonPropertyName("ts")]
+        public string TimeStamp { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        [JsonPropertyName("event_ts")]
+        public string EventTimeStamp { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        [JsonPropertyName("channel_type")]
+        public string ChannelType { get; set; }
+    }
+}
