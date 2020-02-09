4a13f6aa559db062c026032df7b0e6af1e3e2e13
ArisaNiitsuma <d01tsumath@sigmact.com>
2020-02-09 22:23:40 +0900

    他キャラ用のエンドポイント用意

--- SlackCallbackEvent/Configurations/SlackConfiguration.cs
+++ SlackCallbackEvent/Configurations/SlackConfiguration.cs
@@ -6,5 +6,13 @@
     public class SlackConfiguration
     {
         public string HarukaWebhookEndpoint { get; private set; }
+
+        public string MakotoWebhookEndpoint { get; private set; }
+
+        public string RinWebhookEndpoint { get; private set; }
+
+        public string NagisaWebhookEndpoint { get; private set; }
+
+        public string ReiWebhookEndpoint { get; private set; }
     }
 }
